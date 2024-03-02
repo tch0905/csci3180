73eccaf4f5faf550a15cca6a950edc1ab2c31b29
Peter <a1182116615@gmail.com>
2024-03-02 19:06:58 +0800

    move

--- starWar.py
+++ starWar.py
@@ -40,6 +40,7 @@ PLAYER_SIZE_ROWS = 2
 PLAYER_SIZE_COLS = 5
 PLAYER_SPEED = 1
 
+
 # enums
 class DirectionType:
     DIR_UP = 0
@@ -48,13 +49,16 @@ class DirectionType:
     DIR_RIGHT = 3
     DIR_ERROR = 4
 
+
 class BulletType:
     BUL_FROM_ENEMY = 0
     BUL_FROM_PLAYER = 1
 
+
 class GameOverType:
     PLAYER_WIN = 1
     ENEMY_WIN = 2
+
 
 # global variables
 global_directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
@@ -62,6 +66,7 @@ global_game_map = [[' ' for _ in range(GAME_MAP_COLS)] for _ in range(GAME_MAP_R
 global_player = None
 global_enemy = None
 global_bullets = []
+
 
 class Plane:
     def __init__(self, init_location, init_symbol, init_size, init_speed, init_direction, init_life):
@@ -77,7 +82,7 @@ class Plane:
         return self._life
 
     @life.setter
-    def life(self,_life):
+    def life(self, _life):
         self._life = _life
 
     @property
@@ -85,7 +90,7 @@ class Plane:
         return self._symbol
 
     @symbol.setter
-    def symbol(self,_symbol):
+    def symbol(self, _symbol):
         self._symbol = _symbol
 
     @property
@@ -99,25 +104,25 @@ class Plane:
     def draw(self):
         for i in range(self._size[0]):
             for j in range(self._size[1]):
-                global_game_map[(self._location[0] + i) % GAME_MAP_ROWS][(self._location[1] + j) % GAME_MAP_COLS] = self._symbol
+                global_game_map[(self._location[0] + i) % GAME_MAP_ROWS][
+                    (self._location[1] + j) % GAME_MAP_COLS] = self._symbol
 
     def move(self):
-        # TODO: Implement the logic of the next position the plane moves to here.
-        pass
-        
+        self._location = tuple(x + (global_directions[self.direction][y] * self._speed + z) % z
+                               for x, y, z in zip(self._location, (0, 1), (GAME_MAP_ROWS, GAME_MAP_COLS)))
 
     def is_collision(self, bullet_location):
         # TODO: Implement the logic to determine whether the plane has been hit by a bullet here.
         pass
-        
 
     def shoot(self, bullet_location, bullet_direction, bullet_type):
         new_bullet = Bullet(bullet_location, self._symbol, bullet_direction, bullet_type)
         global_bullets.append(new_bullet)
-    
+
     def hit(self):
         self.life -= 1
-    
+
+
 class Bullet:
     def __init__(self, init_location, init_symbol, init_direction, init_type):
         assert init_direction == DirectionType.DIR_UP or init_direction == DirectionType.DIR_DOWN
@@ -135,7 +140,7 @@ class Bullet:
         return self._validity
 
     @validity.setter
-    def validity(self,_validity):
+    def validity(self, _validity):
         self._validity = _validity
 
     def draw(self):
@@ -149,73 +154,71 @@ class Bullet:
             return
 
         # TODO: Implement the logic of the next position the bullet moves to here.
-        
-        
+
         # TODO: Implement the logic to determine whether the bullet hits the plane.
-        
-        
+
         self.check_on_edge()
-    
+
     def check_on_edge(self):
         # TODO: Implement the logic to determine whether the bullet reaches the map boundary.
         pass
-        
+
 
 class Player(Plane):
     def __init__(self, init_location):
-        super().__init__(init_location, '^', (PLAYER_SIZE_ROWS, PLAYER_SIZE_COLS), PLAYER_SPEED, DirectionType.DIR_LEFT, PLAYER_LIFE)
+        super().__init__(init_location, '^', (PLAYER_SIZE_ROWS, PLAYER_SIZE_COLS), PLAYER_SPEED, DirectionType.DIR_LEFT,
+                         PLAYER_LIFE)
         self._shoot_interval = 0
-    
+
     def move(self):
         super().move()
         # TODO: Implement the logic of the player automatically firing a bullet after PLAYER_SHOOT_INTERVAL frames.
-        
 
     def shoot(self):
         bullet_location = (self._location[0], self._location[1] + (self._size[1] // 2))
         super().shoot(bullet_location, DirectionType.DIR_UP, BulletType.BUL_FROM_PLAYER)
-    
+
     def speak(self):
         dialogues = ["I'm ready for action!", "No one can defeat me!", "I'll save the world!"]
         print("Player:\t", random.choice(dialogues))
-        
+
     def display_info(self):
         print("Congratulations! You have defeated them.")
 
 
 class Enemy(Plane):
     def __init__(self, init_location):
-        super().__init__(init_location, '$', (ENEMY_SIZE_ROWS, ENEMY_SIZE_COLS), ENEMY_SPEED, DirectionType.DIR_LEFT, ENEMY_LIFE)
+        super().__init__(init_location, '$', (ENEMY_SIZE_ROWS, ENEMY_SIZE_COLS), ENEMY_SPEED, DirectionType.DIR_LEFT,
+                         ENEMY_LIFE)
         self._shoot_interval = 0
 
     def move(self):
         super().move()
         # TODO: Implement the logic that enemy has a 10% chance of changing direction in each frame.
-        
-        
+
         # TODO: Implement the logic of the player automatically firing a bullet after ENEMY_SHOOT_INTERVAL frames.
-        
 
     def shoot(self):
-        bullet_location = (self._location[0] + self._size[0] - 1, self._location[1] + (self._size[1] // 2))        
+        bullet_location = (self._location[0] + self._size[0] - 1, self._location[1] + (self._size[1] // 2))
         super().shoot(bullet_location, DirectionType.DIR_DOWN, BulletType.BUL_FROM_ENEMY)
-    
+
     def speak(self):
         dialogues = ["You won't escape!", "Prepare to be destroyed!", "I will crush you!"]
         print("Enemy:\t", random.choice(dialogues))
-        
+
     def display_info(self):
         print("Sad! You made sacrifices in this fierce battle.")
-    
+
+
 class Environment:
     def __init__(self):
         self._winner = None
         self._speaker = None
-        
+
         global global_enemy, global_player
         global_enemy = Enemy((0, (GAME_MAP_COLS - ENEMY_SIZE_COLS) // 2))
         global_player = Player((GAME_MAP_ROWS - PLAYER_SIZE_ROWS, (GAME_MAP_COLS - PLAYER_SIZE_COLS) // 2))
-        
+
         # counting down
         print("\n\n\t\tThe game is about to begin!")
         # for i in range(3, 0, -1):
@@ -247,7 +250,7 @@ class Environment:
                 to_be_delete.append(i)
         for idx in reversed(to_be_delete):
             global_bullets.pop(idx)
-        
+
         # TODO: Implement the logic to determine whether the game should end.
         # if global_enemy.life <= 0:
         #     self._winner =
@@ -271,15 +274,15 @@ class Environment:
         for _ in range(GAME_MAP_COLS + 2):
             print('-', end='')
         print()
-        
+
         # TODO: Implement speaking logic here.
         print()
         if random.randint(0, 1) == 0:
             self._speaker = global_player
         else:
             self._speaker = global_enemy
-        self._speaker.speak() 
-        
+        self._speaker.speak()
+
         for _ in range(GAME_MAP_COLS + 2):
             print('-', end='')
         print()
@@ -287,10 +290,9 @@ class Environment:
 
         print("PLAYER\tHP: ", global_player.life)
 
-
     def display_result(self):
         self._winner.display_info()
-    
+
     def get_input(self):
         print("Please select the moving direction! (w : up, s : down, a: left, d: right)")
         player_input = input()  # get input from player
@@ -314,15 +316,9 @@ class Environment:
             self.display_all()
             self.check_state()
         self.display_result()
-    
+
 
 if __name__ == "__main__":
     random.seed(555)
     env = Environment()
     env.run()
-    
-
-
-
-
-
